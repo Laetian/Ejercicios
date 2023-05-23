c75b6ad198688fb1bf21069e4ff94849256668be
Laetian <laetianv@gmail.com>
2023-05-23 12:32:31 +0200

    Update CuentaCorriente.java

--- src/ejercicio3/CuentaCorriente.java
+++ src/ejercicio3/CuentaCorriente.java
@@ -13,7 +13,22 @@ public class CuentaCorriente {
 	public CuentaCorriente(double saldo, String nombreTitular) {
 		this.saldo = saldo;
 		this.nombreTitular = nombreTitular;
-		this.numeroCuenta = (long) Math.random();
+		this.numeroCuenta = (long) Math.random()*99999;
 	}
 
+	
+	public void ingresar(double ingreso) {
+		saldo=saldo+ingreso;
+	}
+	public double consultarSaldo() {
+		return saldo;
+	}
+	public void mostrarInfo() {
+		System.out.println(" Nombre del titular: "+nombreTitular+"/n"
+				+"Saldo: "+saldo+"/n"
+				+"número de cuenta: "+numeroCuenta+"/n");
+	}
+	public void transferencia(CuentaCorriente receptor, CuentaCorriente donante, double monto) {
+		
+	}
 }
