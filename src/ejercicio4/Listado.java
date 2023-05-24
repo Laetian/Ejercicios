ee7a0bc15ba1665deff7f5cb7a28fdf620008f14
Laetian <laetianv@gmail.com>
2023-05-24 14:30:23 +0200

    update

--- src/ejercicio4/Listado.java
+++ src/ejercicio4/Listado.java
@@ -8,13 +8,15 @@ package ejercicio4;
  *
  */
 public class Listado {
-	
+
 	public void mostrarDatos(Persona arrayPersona[]) {
-		for(int i=0;i<arrayPersona.length;i++) {
-			System.out.println("Nombre: "+arrayPersona[i].getNombre());
-			System.out.println("Edad: "+arrayPersona[i].getEdad());
-			if(arrayPersona[i].getEdad()<18) System.out.println(arrayPersona[i].getNombre()+" es menor de edad.");
-			else System.out.println(arrayPersona[i].getNombre()+" es mayor de edad.");
+		for (int i = 0; i < arrayPersona.length; i++) {
+			System.out.println("Nombre: " + arrayPersona[i].getNombre());
+			System.out.println("Edad: " + arrayPersona[i].getEdad());
+			if (arrayPersona[i].getEdad() < 18)
+				System.out.println(arrayPersona[i].getNombre() + " es menor de edad.");
+			else
+				System.out.println(arrayPersona[i].getNombre() + " es mayor de edad.\n");
 		}
 	}
 
--- src/ejercicio4/Main.java
+++ src/ejercicio4/Main.java
@@ -6,14 +6,14 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		Persona[] arrayPersona = new Persona[5];
-		
-		arrayPersona[0]=new Persona(15, "Anna");
-		arrayPersona[1]=new Persona(19, "Juan");
-		arrayPersona[2]=new Persona(15, "Luis");
-		arrayPersona[3]=new Persona(15, "Maca");
-		arrayPersona[4]=new Persona(15, "Pepe");
-		
-		Listado listado= new Listado();
+
+		arrayPersona[0] = new Persona(15, "Anna");
+		arrayPersona[1] = new Persona(19, "Juan");
+		arrayPersona[2] = new Persona(41, "Luis");
+		arrayPersona[3] = new Persona(99, "Maca");
+		arrayPersona[4] = new Persona(7, "Pepe");
+
+		Listado listado = new Listado();
 		listado.mostrarDatos(arrayPersona);
 	}
 
